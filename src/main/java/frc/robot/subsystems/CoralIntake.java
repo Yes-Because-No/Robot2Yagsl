f67bbe374bec17bfeb42a192a77da5661137801d
terryzhang180@gmail.com <115563034+coldsus@users.noreply.github.com>
2025-02-07 18:22:05 -0500

    Finished up the CoralIntake systems

--- src/main/java/frc/robot/subsystems/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/CoralIntake.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.techhounds.houndutil.houndlib.subsystems.BaseIntake;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -8,18 +11,67 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /** The subsystem for the robot's coral intake and scoring mechanism */
 public class CoralIntake extends SubsystemBase implements BaseIntake {
     public static final class Constants {
+        //CAN IDS for Spark Maxes
+        public static final class CAN_IDS{
+            public static int MOTOR_L = 20;
+            public static int MOTOR_R = 21;
 
+        }
+        public static final double VOLTAGE = 0; //ToDo: Find the correct voltage
+        public static final class INVERSIONS {
+            public static final boolean MOTOR_L = true;//ToDo: Find the correct Inversions
+            public static final boolean MOTOR_R = false;
+        }
+        public static final class CURRENT_LIMIT{
+            public static final int MOTOR_L = 0;//ToDo: Find the correct current limit
+            public static final int MOTOR_R = 0;
+
+        }
     }
     
+    private static SparkMax motorL = new SparkMax(Constants.CAN_IDS.MOTOR_L, MotorType.kBrushless);
+    private static SparkMaxConfig motorLConfig = new SparkMaxConfig();
+
+    /** The constructor for the coralIntake subsystem
+     * @return the object
+    */
+    public CoralIntake(){
+        motorLConfig
+        .smartCurrentLimit(Constants.CURRENT_LIMIT.MOTOR_L)
+        .inverted(Constants.INVERSIONS.MOTOR_L);
+
+        motorRConfig
+        .smartCurrentLimit(Constants.CURRENT_LIMIT.MOTOR_R)
+        .inverted(Constants.INVERSIONS.MOTOR_R);
+    }
+    private static SparkMax motorR = new SparkMax(Constants.CAN_IDS.MOTOR_R, MotorType.kBrushless);
+    private static SparkMaxConfig motorRConfig = new SparkMaxConfig();
+
+    /** Runs the rollers of the coral intake mechanism forward 
+     * @return the command
+    */
     @Override
     public Command runRollersCommand() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'runRollersCommand'");
-    }
+        return startEnd (() ->{
+            motorL.setVoltage(Constants.VOLTAGE);
+            motorR.setVoltage(Constants.VOLTAGE);
+        }, () -> {
+            motorL.stopMotor();
+            motorR.stopMotor();
+        });
 
+    }
+    /** Runs the rollers of the coral intake mechanism in reverse
+    * @return the command
+    */
     @Override
     public Command reverseRollersCommand() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reverseRollersCommand'");
+        return startEnd (() ->{
+            motorL.setVoltage(-Constants.VOLTAGE);
+            motorR.setVoltage(-Constants.VOLTAGE);
+        }, () -> {
+            motorL.stopMotor();
+            motorR.stopMotor();
+        });
     }
 }
