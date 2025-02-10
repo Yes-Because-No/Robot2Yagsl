cdcccf0e225388f64f926bcb882aec5e623680f6
Yes-Because-No <alex.tichindelean@gmail.com>
2025-02-10 17:10:06 -0500

    Add basic config and motor object

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,18 +2,33 @@ package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.techhounds.houndutil.houndlib.subsystems.BaseLinearMechanism;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Elevator.Constants.Position;
+import frc.robot.subsystems.Elevator.Constants.*;
 
 /** The subsystem for the robot's elevator mechanism */
 public class Elevator extends SubsystemBase implements BaseLinearMechanism<Position> {
     public static final class Constants {
+        public static final class CAN{
+            public static final int CANID = 0;
+        }
+
+        public static final class MotorConfigs{
+            public static boolean INVERTED = false;
+            public static double CURRENT_LIMIT = 0.0;
+            public static double ENCODER_CONVERSION_FACTOR = 0.0;
+        }
 
         public static enum Position {
-            ZERO(0.0);
+            ZERO(0.0),
+            L1(0.0),
+            L2(0.0),
+            L3(0.0),
+            L4(0.0);
 
             public final double position;
 
@@ -22,6 +37,8 @@ public class Elevator extends SubsystemBase implements BaseLinearMechanism<Posit
             }
         }
     }
+
+    private final SparkMax elevatorMotor = new SparkMax(CAN.CANID, MotorType.kBrushless);
 
     @Override
     public double getPosition() {
