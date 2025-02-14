7a02be5f9d53c1a1d4df5fc61dbf184449ae0945
QuinnKenison <quinnkenison@gmail.com>
2025-02-14 13:34:03 -0500

    Format

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -39,15 +39,15 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         }
 
         public static final class CURRENT_LIMITS {
-            public static final int ARM_L = 0; //TODO
-            public static final int ARM_R = 0; //TODO
-            public static final int BAR = 0; //TODO
+            public static final int ARM_L = 0; // TODO
+            public static final int ARM_R = 0; // TODO
+            public static final int BAR = 0; // TODO
         }
 
         public static final class INVERSION {
-            public static final boolean ARM_L = true; //TODO after testing
-            public static final boolean ARM_R = false; //TODO after testing
-            public static final boolean BAR = false; //TODO after testing
+            public static final boolean ARM_L = true; // TODO after testing
+            public static final boolean ARM_R = false; // TODO after testing
+            public static final boolean BAR = false; // TODO after testing
         }
 
         public static final class GEAR_RATIOS {
@@ -56,24 +56,24 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         }
 
         public static final class Feedback {
-            public static final double kP = 0; //TODO SysId
-            public static final double kI = 0; //TODO SysId
-            public static final double kD = 0; //TODO SysId
-            public static final double MAX_VELOCITY = 0; //TODO SysId
-            public static final double MAX_ACCEL = 0; //TODO SysId
+            public static final double kP = 0; // TODO SysId
+            public static final double kI = 0; // TODO SysId
+            public static final double kD = 0; // TODO SysId
+            public static final double MAX_VELOCITY = 0; // TODO SysId
+            public static final double MAX_ACCEL = 0; // TODO SysId
         }
 
         public static final class Feedforward {
-            public static final double kS = 0; //TODO SysId
-            public static final double kG = 0; //TODO SysId
-            public static final double kV = 0; //TODO SysId
-            public static final double kA = 0; //TODO SysId
+            public static final double kS = 0; // TODO SysId
+            public static final double kG = 0; // TODO SysId
+            public static final double kV = 0; // TODO SysId
+            public static final double kA = 0; // TODO SysId
         }
 
-        public static final double VOLTAGE = 0; //TODO find desired speed
+        public static final double VOLTAGE = 0; // TODO test for desired speed of bar
 
         public static enum Position {
-            ARM_RESET(0.0); //TODO test for actual position
+            ARM_RESET(0.0); // TODO test for actual position
 
             public final double position;
 
@@ -98,9 +98,11 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
     private final SparkMaxConfig barConfig = new SparkMaxConfig();
 
     // Create controller objects
-    private final ProfiledPIDController PID = new ProfiledPIDController(Constants.Feedback.kP, Constants.Feedback.kI, Constants.Feedback.kD, 
+    private final ProfiledPIDController PID = new ProfiledPIDController(Constants.Feedback.kP, Constants.Feedback.kI,
+        Constants.Feedback.kD,
         new TrapezoidProfile.Constraints(Constants.Feedback.MAX_VELOCITY, Constants.Feedback.MAX_ACCEL));
-    private final ArmFeedforward Feedforward = new ArmFeedforward(Constants.Feedforward.kS, Constants.Feedforward.kG, Constants.Feedforward.kV, Constants.Feedforward.kA);
+    private final ArmFeedforward Feedforward = new ArmFeedforward(Constants.Feedforward.kS, Constants.Feedforward.kG,
+        Constants.Feedforward.kV, Constants.Feedforward.kA);
 
     // SysId stuffs
     private final MutVoltage sysIdVoltage = Volts.mutable(0);
@@ -115,14 +117,16 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
             },
             (log) -> {
                 log.motor("AlgaeIntakeArm").voltage(sysIdVoltage.mut_replace(getVoltage(), Volts))
-                        .angularPosition(sysIdAngle.mut_replace(getPosition(), Degrees))
-                        .angularVelocity(sysIdAngularVelocity.mut_replace(getVelocity(), DegreesPerSecond));
+                    .angularPosition(sysIdAngle.mut_replace(getPosition(), Degrees))
+                    .angularVelocity(sysIdAngularVelocity.mut_replace(getVelocity(), DegreesPerSecond));
             },
             this
         )
     );
 
-    /** The constructor for the coral intake subsystem
+    /**
+     * The constructor for the coral intake subsystem
+     * 
      * @return the object
      */
     public AlgaeIntake() {
@@ -134,7 +138,7 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
             .encoder
                 .positionConversionFactor(Constants.GEAR_RATIOS.ARM * (2 * Math.PI))
                 .velocityConversionFactor((Constants.GEAR_RATIOS.ARM * (2 * Math.PI)) / 60);
-        
+
         armRConfig
             .smartCurrentLimit(Constants.CURRENT_LIMITS.ARM_R)
             .idleMode(IdleMode.kBrake)
@@ -155,7 +159,9 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         PID.reset(getPosition());
     }
 
-    /** Gets the position of the algae intake arm mechanism
+    /**
+     * Gets the position of the algae intake arm mechanism
+     * 
      * @return the position in radians
      */
     @Override
@@ -165,17 +171,19 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
 
     /**
      * Gets the velocity of the {@link #armLEncoder encoder}
+     * 
      * @return The velocity, in degrees per second, as a double
      */
-    public double getVelocity(){
+    public double getVelocity() {
         return armLEncoder.getVelocity();
     }
 
     /**
      * Gets the voltage applied to the {@link #armL motor}
-     * @return Voltage applied to the motor, as a double
+     * 
+     * @return voltage applied to the motor, as a double
      */
-    public double getVoltage(){
+    public double getVoltage() {
         return armL.getAppliedOutput();
     }
 
@@ -186,27 +194,34 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         armREncoder.setPosition(Constants.Position.ARM_RESET.position);
     }
 
-    /** Explicitly set the voltage of the algae intake arm mechanism 
+    /**
+     * Explicitly set the voltage of the algae intake arm mechanism
+     * 
      * @param voltage to set [-12, 12]
-    */
+     */
     @Override
     public void setVoltage(double voltage) {
         armL.setVoltage(MathUtil.clamp(voltage, -12, 12));
         armR.setVoltage(MathUtil.clamp(voltage, -12, 12));
     }
 
-    /** Move the algae intake arm mechanism to it's current goal position
+    /**
+     * Move the algae intake arm mechanism to it's current goal position
+     * 
      * @return the command
      */
     @Override
     public Command moveToCurrentGoalCommand() {
         return run(() -> {
-            setVoltage(PID.calculate(getPosition()) + Feedforward.calculate(PID.getGoal().position, PID.getGoal().velocity));
+            setVoltage(PID.calculate(getPosition())
+                + Feedforward.calculate(PID.getGoal().position, PID.getGoal().velocity));
         }).withName("AlgaeIntake.moveToCurrentGoalCommand");
     }
 
-    /** Move the algae intake arm mechanism to a defined position by
+    /**
+     * Move the algae intake arm mechanism to a defined position by
      * setting the goal and moving to the goal until reached
+     * 
      * @param goalPositionSupplier a supplier of an instance of the position enum
      * @return the command
      */
@@ -220,11 +235,13 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         ).withName("AlgaeIntake.moveToPositionCommand");
     }
 
-    /** Move the algae intake arm mechanism to any position by
+    /**
+     * Move the algae intake arm mechanism to any position by
      * setting the goal and moving to the goal until reached
+     * 
      * @param goalPositionSupplier a supplier of the position in radians
      * @return the command
-    */
+     */
     @Override
     public Command moveToArbitraryPositionCommand(Supplier<Double> goalPositionSupplier) {
         return Commands.sequence(
@@ -235,11 +252,13 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         ).withName("AlgaeIntake.moveToArbitraryPositionCommand");
     }
 
-    /** Move the algae intake arm mechanism to the current position plus a delta by
+    /**
+     * Move the algae intake arm mechanism to the current position plus a delta by
      * setting the goal and moving to the goal until reached
+     * 
      * @param delta a supplier of the delta in radians
      * @return the command
-    */
+     */
     @Override
     public Command movePositionDeltaCommand(Supplier<Double> delta) {
         return Commands.sequence(
@@ -250,10 +269,12 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         ).withName("AlgaeIntake.movePositionDeltaCommand");
     }
 
-    /** Hold the algae intake arm mechanism at it's current position by
+    /**
+     * Hold the algae intake arm mechanism at it's current position by
      * setting the goal to the current goal and moving toward it until cancelled
+     * 
      * @return the command
-    */
+     */
     @Override
     public Command holdCurrentPositionCommand() {
         return Commands.sequence(
@@ -264,9 +285,11 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         ).withName("AlgaeIntake.holdCurrentPositionCommand");
     }
 
-    /** Instantly set reset the encoder positions of the algae intake arm mechanism 
+    /**
+     * Instantly reset the encoder positions of the algae intake arm mechanism
+     * 
      * @return the command
-    */
+     */
     @Override
     public Command resetPositionCommand() {
         return runOnce(() -> {
@@ -274,8 +297,10 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         }).withName("resetPositionCommand");
     }
 
-    /** Explicitly set the speed of the algae intake arm mechanism,
+    /**
+     * Explicitly set the speed of the algae intake arm mechanism,
      * overridding PID & feedforward control
+     * 
      * @param speed the speed [-1, 1]
      * @return the command
      */
@@ -288,8 +313,11 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         }).withName("AlgaeIntake.setOverridenSpeedCommand");
     }
 
-    /** Coast the motors of the robot for manual movement by
-     * stopping them, setting them to coast, and then setting them back to break on command end
+    /**
+     * Coast the motors of the robot for manual movement by
+     * stopping them, setting them to coast, and then setting them back to break on
+     * command end
+     * 
      * @return the command
      */
     @Override
@@ -310,7 +338,10 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
         });
     }
 
-    /** Run the bar of the algae intake at a predetermined voltage in the direction that will intake an algae
+    /**
+     * Run the bar of the algae intake at a predetermined voltage in the direction
+     * that will intake an algae
+     * 
      * @return the command
      */
     @Override
@@ -319,10 +350,13 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
             bar.setVoltage(Constants.VOLTAGE);
         }, () -> {
             bar.stopMotor();
-        });
+        }).withName("AlgaeIntake.runRollersCommand");
     }
 
-    /** Run the bar of the algae intake at a predetermined voltage in the direction that will eject an algae
+    /**
+     * Run the bar of the algae intake at a predetermined voltage in the direction
+     * that will eject an algae
+     * 
      * @return the command
      */
     @Override
@@ -331,27 +365,30 @@ public class AlgaeIntake extends SubsystemBase implements BaseIntake, BaseSingle
             bar.setVoltage(-Constants.VOLTAGE);
         }, () -> {
             bar.stopMotor();
-        });
+        }).withName("AlgaeIntake.reverseRollersCommand");
     }
 
     /**
      * Creates a command for the sysId quasistatic test, which gradually speeds up
      * the mechanism to eliminate variation from acceleration
+     * 
      * @see https://docs.wpilib.org/en/stable/docs/software/advanced-controls/system-identification/creating-routine.html
      * @param direction Direction to run the motors in
      * @return Command that runs the quasistatic test
      */
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction){
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
         return sysIdRoutine.quasistatic(direction);
     }
+
     /**
      * Creates a command for the sysId dynamic test, which will step up the speed to
      * see how the mechanism behaves during acceleration
+     * 
      * @see https://docs.wpilib.org/en/stable/docs/software/advanced-controls/system-identification/creating-routine.html
      * @param direction Direction to run the motors in
      * @return Command that runs the dynamic test
      */
-    public Command sysIdDynamic(SysIdRoutine.Direction direction){
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
         return sysIdRoutine.dynamic(direction);
     }
 }
