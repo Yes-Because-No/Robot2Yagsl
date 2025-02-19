1bbb0e42705883dda107c6c05b93caff4547a5c1
RajProspects <79417360+TehYeetGod@users.noreply.github.com>
2025-02-19 16:33:27 -0500

    Add initial

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,8 +1,19 @@
 package frc.robot;
 
-/**
- * Controls configurations for robot.
- */
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.CoralIntake;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.AlgaeIntake;
+import frc.robot.subsystems.Climber;
+
+import com.techhonds.houndutil.houndlib.oi.CommandVirpilJoystick;
+
+
 public class Controls {
     
+    private static boolean inCoralMode = true; //set to coral mode as default
+    private static boolean alignLeft = false; //align first to the right
+    private static int level = 1;
+    private static boolean isClimbing = false;
+
 }
