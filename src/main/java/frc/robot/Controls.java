3e977fd3ac2f82cd332a92a154e8e38acf620fb3
RajProspects <79417360+TehYeetGod@users.noreply.github.com>
2025-02-20 17:13:46 -0500

    Add Initial Controls Layout

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,8 +1,32 @@
 package frc.robot;
 
-/**
- * Controls configurations for robot.
- */
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.CoralIntake;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.AlgaeIntake;
+import frc.robot.subsystems.Climber;
+
+import com.techhounds.houndutil.houndlib.oi.CommandVirpilJoystick;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+
 public class Controls {
     
+    private static boolean inCoralMode = true; //set to coral mode as default
+    private static boolean alignLeft = false; //align first to the right
+    private static int level = 1;
+    private static boolean isClimbing = false;
+
+    private static void configureOperatorControls(int port, Drivetrain drivetrain, CoralIntake coralintake, Elevator elevator, AlgaeIntake algaeintake, Climber climber){
+        
+        CommandXboxController controller = new CommandXboxController(port);
+        
+        
+        
+    }
+
+
+
+
 }
