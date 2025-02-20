f5454861a085429a21043ffef4bf58f9b099de92
Remzy <145932877+Remzy006@users.noreply.github.com>
2025-02-20 17:00:02 -0500

    Revert "Add initial"
    This reverts commit 1bbb0e42705883dda107c6c05b93caff4547a5c1.

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,19 +1,8 @@
 package frc.robot;
 
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.CoralIntake;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.AlgaeIntake;
-import frc.robot.subsystems.Climber;
-
-import com.techhonds.houndutil.houndlib.oi.CommandVirpilJoystick;
-
-
+/**
+ * Controls configurations for robot.
+ */
 public class Controls {
     
-    private static boolean inCoralMode = true; //set to coral mode as default
-    private static boolean alignLeft = false; //align first to the right
-    private static int level = 1;
-    private static boolean isClimbing = false;
-
 }
